0a7a7940cdaa7ecfa5509e08eec32f63c023560c
GerardGB <gerardgb9@gmail.com>
2019-09-20 16:23:09 +0200

    WIP

---
+++ js/app.js
@@ -0,0 +1,21 @@
+//console.log('Hola mon');
+
+console.log(document);
+
+console.log('document.body');
+
+const name = 'Gerard';
+
+import greeting from './js/templates/greeting.js'
+
+console.log(greeting);
+
+/* const template = `
+<h1>Hola ${name}</h1>
+`; */
+
+//document.body.innerHTML = "Hola "+ name;
+
+document.body.innerHTML = template;
+
+// import i export
