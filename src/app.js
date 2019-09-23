6db23cdd105b79544926a97cdb472ace999a2ee2
GerardGB <gerardgb9@gmail.com>
2019-09-23 19:38:00 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,21 +1,5 @@
-//console.log('Hola mon');
-
-console.log(document);
-
-console.log('document.body');
-
 const name = 'Gerard';
 
-import greeting from './js/templates/greeting.js.js'
+import greeting from './js/templates/greeting'
 
-console.log(greeting);
-
-/* const template = `
-<h1>Hola ${name}</h1>
-`; */
-
-//document.body.innerHTML = "Hola "+ name;
-
-document.body.innerHTML = template;
-
-// import i export
+document.body.innerHTML = greeting;
