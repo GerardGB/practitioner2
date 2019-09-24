77e3537375ce6588fd53560eba9fdd6f784e7abe
GerardGB <gerardgb9@gmail.com>
2019-09-24 19:23:23 +0200

    WIP

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const name = 'Gerard';
 
-import greeting from './templates/greeting'
-
+//import greeting from './templates/greeting'
+const greeting = `
+<h1>Hola ${name}</h1>
+`;
 
 console.log(greeting);
 document.body.innerHTML = greeting;
