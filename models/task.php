000fa4ef89778231a9bf1c18f7b577992d7e0d37
GerardGB <gerardgb9@gmail.com>
2019-09-25 21:31:03 +0200

    WIP

--- models/task.php
+++ models/task.php
@@ -1,6 +1,25 @@
 <?php
-$task = [
+
+try {
+$pdo = new PDO('mysql:host=127.0.0.1;dbname=tasks','debian-sys-maint','fgKo0I75jriiw1Qq');
+} catch (PDOException $e) {
+    die($e->getMessage());
+}
+
+//die(var_dump($pdo));
+
+$query = $pdo -> prepare('SELECT * FROM tasks');
+
+$query -> execute();
+
+$results = $query -> fetchAll();
+
+//die(var_dump($results[1]));
+
+$task = $results[0];
+
+/* $task = [
     'name' => 'Comprar llet',
     'description' => 'Al Lidl...',
     'completed' => false,
-];
+]; */
